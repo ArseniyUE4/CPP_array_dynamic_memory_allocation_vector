f4d143f222a5cf9c411e38f9e854aa9f43fe772e
ArseniyUE4 <91388672+ArseniyUE4@users.noreply.github.com>
2021-09-25 22:57:49 +0300

    ArseniyUE4

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//array
+
+int array[] = { 1,2,3,4,5 };
+
+//dynamic memory allocation
+
+int cont;
+int* nums = new int[cont];
+
+//vector
+
+vector<int> numbers;
+
